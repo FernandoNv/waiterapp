5b1d96fd78e620cd4e8cd627049a22a160a5b3da
Fernando Vieira <fernandonvieira47@hotmail.com>
2022-07-03 21:23:33 -0300

    project created

---
+++ src/test/java/com/example/waiterapp/WaiterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.waiterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaiterAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
