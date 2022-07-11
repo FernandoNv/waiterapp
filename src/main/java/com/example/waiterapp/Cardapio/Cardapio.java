8f08de4dbb00982ca0aa8ed17f3933f3e42c6bd2
Fernando Vieira <fernandonvieira47@hotmail.com>
2022-07-11 12:10:33 -0300

    branch com os arquivos da entrega 2

--- src/main/java/com/example/waiterapp/Cardapio/Cardapio.java
+++ src/main/java/com/example/waiterapp/Cardapio/Cardapio.java
@@ -1,10 +1,6 @@
 package com.example.waiterapp.Cardapio;
 
 import com.example.waiterapp.Item.Item;
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
